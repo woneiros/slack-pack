f65c841ab273e4dd814d1aae06662d37a507a173
karzak <kjydavis3@gmail.com>
2016-10-29 18:21:25 -0400

    Kafka installer script

--- env/kafka_remote_install.sh
+++ env/kafka_remote_install.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update -y
 sudo apt-get install oracle-java8-installer -y
 
-sudo echo 'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> /etc/environment
+echo 'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' | sudo tee -a /etc/environment
 sudo source /etc/environment
 
 wget http://ftp.wayne.edu/apache/kafka/0.10.0.0/kafka_2.11-0.10.0.0.tgz
