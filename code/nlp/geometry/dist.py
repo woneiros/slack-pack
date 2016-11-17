1bf358fcbd76554384d6b66a35cfad58bc639f9a
WillahScott <willie9monge@gmail.com>
2016-11-16 23:18:14 -0500

    Added cosine similarity, changed distance to similarity in SimilarityTopicCalculator

--- code/nlp/geometry/dist.py
+++ code/nlp/geometry/dist.py
@@ -2,6 +2,10 @@
 
 """Implementation of different distances to use for a geometry
 
+Note
+----
+The following are in fact _similarities_ and not distances (a score of 1 means very similar, a score of 0 means very different)
+
 .. module:: nlp.geometry.dist
    :platform: Unix, Windows
    :synopsis: Distances between message representations
@@ -12,20 +16,23 @@
 
 """
 
+import numpy as np
 
-class CosineDistance(object):
-    """Cosine distance between two words
 
+def cosine(repr1, repr2):
+    """Calculate the `cosine similarity<https://en.wikipedia.org/wiki/Cosine_similarity>`_ between two object representations
 
     Parameters
     ----------
-    representation : TYPE
-        Description
+    repr1 : list[float]
+        Representation of object 1
+    repr2 : list[float]
+        Representation of object 2
 
+    Returns
+    -------
+    float
+        Distance/Similarity between the two object representations
     """
+    return (np.array(repr1) * np.array(repr2)).sum() / (np.linalg.norm(repr1) * np.linalg.norm(repr2))
 
-    def __init__(self, representation):
-        """Summary
-
-        """
-        self.arg = arg
--- code/nlp/models/similarity_calculation.py
+++ code/nlp/models/similarity_calculation.py
@@ -26,7 +26,7 @@ class SimilarTopicCalculator:
     ----------
     representation : callable
         Callable that returns a messages representation
-    distance : callable
+    similarity : callable
         Callable that returns a float when called over two representations
     tokenizer : callable, optional
         Message tokenizer
@@ -34,17 +34,17 @@ class SimilarTopicCalculator:
     Raises
     ------
     AttributeError
-        If representation or distance are not callable objects. Will also raise if tokenizer is not None nor callable.
+        If representation or similarity are not callable objects. Will also raise if tokenizer is not None nor callable.
     """
 
-    def __init__(self, representation, distance, tokenizer=None):
+    def __init__(self, representation, similarity, tokenizer=None):
         if not hasattr(representation, '__call__'):
             raise AttributeError('representation needs to be a callable object')
         self.representation = representation
 
-        if not hasattr(distance, '__call__'):
-            raise AttributeError('distance needs to be a callable object')
-        self.distance = distance
+        if not hasattr(similarity, '__call__'):
+            raise AttributeError('similarity needs to be a callable object')
+        self.similarity = similarity
 
         if (tokenizer is not None) and not hasattr(tokenizer, '__call__'):
             raise AttributeError('tokenizer needs to be a callable object or None')
@@ -81,23 +81,12 @@ class SimilarTopicCalculator:
         for topic in self.window.topics:
 
             # get centroid of the topic
-            self.calculate_centroid(topic)
+            centroid = self.calculate_centroid(topic)
 
-            # get distance to centroid
-            similarities.append( self.distance(centroid, messages.text_repr) )  # NOTE: distance to similarity!
+            # get similarity with centroid
+            similarities.append( self.similarity(centroid, messages.text_repr) )
 
         return similarities
-
-        #     for topic_message in topic.messages:
-        #         (centroidDistance, cosine) = self.model.calculateSimilarity(
-        #             message, topic_message, message.getID() - topic_message.getID())
-        #         similarities.append(TopicSimilarity(topic, cosine, centroidDistance))
-        # similarities.sort(key=lambda x: x.getCentroidDistance())
-        # # get top 5 percent
-        # size = int(math.ceil(len(similarities) * 5. / 100))
-        # similarities = similarities[0:size]
-        # similarities.sort(key= lambda x: -x.getScore())
-        # return None if len(similarities) == 0 else similarities[0]
 
     def calculate_centroid(self, topic):  # NOTE: might want to implement top 5% later...
         """Calculate the centroid of the topic based on the message representations
@@ -160,52 +149,3 @@ class TopicSimilarity:
         self.topic = topic
         self.score = score
         self.centroidDistance = centroidDistance
-
-
-
-class Model:
-    def __init__(self, messages, tokenizer):
-        self.tokenizer = tokenizer
-        self.innerModel = self.trainNewModel(messages)
-        self.index2wordSet = set(self.innerModel.index2word)
-
-    def trainNewModel(self, messages):
-        return gensim.models.Word2Vec(
-            [self.tokenizer.stemAndTokenize(message) for message in messages],
-            min_count = 1)
-
-    def __getitem__(self, index):
-        return self.innerModel[index]
-
-    def calculateSimilarity(self, messageA, messageB, indexDistance):
-        fullTokensA = self.tokenizer.stemAndTokenize(messageA)
-        fullTokensB = self.tokenizer.stemAndTokenize(messageB)
-
-        width = 10
-        startA = 0
-        best = (float('inf'), 0) # orthogonal
-        decay = (0.993 ** indexDistance) # must be related to the cosine threshold
-        while startA < len(fullTokensA):
-            startB = 0
-            tokensA = fullTokensA[startA:(startA + width)]
-            while startB < len(fullTokensB):
-                tokensB = fullTokensB[startB:(startB + width)]
-                cosine = self.innerModel.n_similarity(tokensA, tokensB) * decay
-                centroid = self.centroidDistance(tokensA, tokensB) / decay
-                pair = (centroid, cosine)
-                if best is None or best > pair:
-                    best = pair
-                startB = startB + width / 2
-            startA = startA + width / 2
-        return best
-
-    def centroidDistance(self, tokensA, tokensB):
-        centroidA = sum([self[t] for t in tokensA]) / len(tokensA)
-        centroidB = sum([self[t] for t in tokensB]) / len(tokensB)
-        return np.linalg.norm(self.centroid(tokensA) - self.centroid(tokensB))
-
-    def centroid(self, tokens):
-        return sum([self[t] for t in tokens]) / len(tokens)
-
-
-
